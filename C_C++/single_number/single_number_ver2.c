ea2109bc49c219be7089c964566acce232b765f2
davidho27941 <jimsam3456@cycu.org.tw>
2020-06-19 01:40:23 +0800

    [Add] version 2

---
+++ C_C++/single_number/single_number_ver2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Single(int num, int array[]){
+    int n = array[0];
+    for(int i=1 ; i < num ; i++){
+   	n ^= array[i];
+    }
+    return n;
+}
+
+int main(void){
+	int a;
+	printf("Please input the lenght of array: \n");
+	scanf("%d", &a);
+   	int arr[a] ;
+   	printf("Your array's lenght is : %d \n", a);
+   	printf("Please input your element. \n");
+   	for(int i=0;i<a;i++){
+       		scanf("%d", &arr[i]);
+    	}
+	
+	int result = Single( a, arr);
+	printf("result: %d \n", result);
+	return 0;
+}
